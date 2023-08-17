284f6b110a53bdf98a4b26379aaf751f38203376
SERHIIBZENKD8F4\serhiibzenko <bzenkosergey@gmail.com>
2023-08-17 13:08:44 +0200

    deploy

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -13,6 +14,10 @@ import { AppComponent } from './app.component';
     AppRoutingModule
   ],
   declarations: [AppComponent],
+  providers: [{
+    provide: LocationStrategy,
+    useClass: HashLocationStrategy
+  }],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
